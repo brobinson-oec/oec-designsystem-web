0404c9c452e9f699ac3be0013d83e6b261b1e50a
brobinson-oec <111073307+brobinson-oec@users.noreply.github.com>
2022-10-17 10:30:27 -0400

    Add files via upload

---
+++ build/UserProfileService-ea1aea9c.js
@@ -0,0 +1,43 @@
+class UserProfileService {
+  getInfo(uid) {
+    return Promise.resolve({
+      id: uid,
+      displayName: "John McDonald",
+      orgName: "Toyota of Richfield",
+      profilePictureUri: ""
+    });
+  }
+  getUserById(uid) {
+    return Promise.resolve(USERS.filter(function (x) {
+      return x.id === uid;
+    })[0]);
+  }
+}
+const USERS = [
+  {
+    id: "abc123",
+    displayName: "Joe McDonald",
+    orgName: "Toyota of Richfield",
+    profilePictureUri: ""
+  },
+  {
+    id: "abc456",
+    displayName: "Ivan Terrible",
+    orgName: "Toyota of Richfield",
+    profilePictureUri: ""
+  },
+  {
+    id: "xyz123",
+    displayName: "Allan Street",
+    orgName: "Toyota of Richfield",
+    profilePictureUri: ""
+  },
+  {
+    id: "xyz456",
+    displayName: "Andrew Cobol",
+    orgName: "Toyota of Richfield",
+    profilePictureUri: ""
+  }
+];
+
+export { UserProfileService as U };
--- build/oec-user-info.entry.js
+++ build/oec-user-info.entry.js
@@ -1,8 +1,8 @@
 import { h, r as registerInstance, i as getAssetPath, f as Host } from './index-1f24ee20.js';
-import { R as ReplaySubject, S as Subject, c as debounceTime, s as switchMap, o as of, f as from, t as takeUntil } from './index-6faee293.js';
-import './index-c1978730.js';
-import { U as UserProfileService } from './UserProfileService-69351221.js';
-import { m as myContainer, T as TYPES } from './inversify.config-74d84e0a.js';
+import { R as ReplaySubject, S as Subject, c as debounceTime, s as switchMap, o as of, f as from, t as takeUntil } from './index-73b9da7f.js';
+import './index-b727d372.js';
+import { U as UserProfileService } from './UserProfileService-ea1aea9c.js';
+import { m as myContainer, T as TYPES } from './inversify.config-46a3801b.js';
 
 const PencilIcon = (attrs) => (h("svg", Object.assign({ xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 512 512" }, attrs),
   h("path", { d: "M421.7 220.3l-11.3 11.3-22.6 22.6-205 205c-6.6 6.6-14.8 11.5-23.8 14.1L30.8 511c-8.4 2.5-17.5 .2-23.7-6.1S-1.5 489.7 1 481.2L38.7 353.1c2.6-9 7.5-17.2 14.1-23.8l205-205 22.6-22.6 11.3-11.3 33.9 33.9 62.1 62.1 33.9 33.9zM96 353.9l-9.3 9.3c-.9 .9-1.6 2.1-2 3.4l-25.3 86 86-25.3c1.3-.4 2.5-1.1 3.4-2l9.3-9.3H112c-8.8 0-16-7.2-16-16V353.9zM453.3 19.3l39.4 39.4c25 25 25 65.5 0 90.5l-14.5 14.5-22.6 22.6-11.3 11.3-33.9-33.9-62.1-62.1L314.3 67.7l11.3-11.3 22.6-22.6 14.5-14.5c25-25 65.5-25 90.5 0z" })));
